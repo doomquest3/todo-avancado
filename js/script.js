6883faec8ed16970f66484551241d655555a6130
Leandro de Jesus <lean_silva100@hotmail.com>
2023-06-07 18:46:37 -0300

    3º commit, adicionado mudar tarefas, podendo ser feito no html e no localstorage.

--- js/script.js
+++ js/script.js
@@ -1,23 +1,39 @@
 // Seleção de elementos
 const todoForm = document.querySelector("#todo-form");
-
 const todoInput = document.querySelector("#todo-input");
 
 const todoList = document.querySelector("#todo-list");
 
 const editForm = document.querySelector("#edit-form");
-
 const editInput = document.querySelector("#edit-input");
 
 const cancelEditBtn = document.querySelector("#cancel-edit-btn");
 
 let oldInputValue;
+var item;
+
+
+
+
 
 // Funções
+// Localizar item localstorage
+const localizarItem = (text) =>{
+    let n = Object.keys(localStorage);
+
+    for(let i of n){
+        if(text.localeCompare(localStorage.getItem(i))===0){
+            console.log(i);
+            return i;
+        }
+
+    }
+
+}
+
 // Salvar dados na local storage
 const saveLocalStorage = (text) =>{
-    let num = localStorage.length += 1;
-
+    let num = localStorage.length;
     localStorage.setItem(num, text);
 
 }
@@ -25,7 +41,6 @@ const saveLocalStorage = (text) =>{
 // Recuperar dados da local storage.
 const recuperarDados = () =>{
     let n = Object.keys(localStorage);
-
     for(let i of n){
         let textDate = localStorage.getItem(i);
         saveTodo(textDate);
@@ -37,17 +52,15 @@ const recuperarDados = () =>{
 //deletar item da local storage.
 
 const deletarDado = (text) =>{    
-    let n = Object.keys(localStorage);
-    for(let i of n){
-        let resultado = text.localeCompare(localStorage.getItem(i));
-        if(resultado === 0){
-            console.log(i)
-            localStorage.removeItem(i)
-
-        }
-
-    }
+    let i = localizarItem(text);
+    localStorage.removeItem(i);
     
+}
+
+//Trocar item na localstorage.
+const substituirItem = (num, text) =>{
+    localStorage.setItem(num, text);
+
 }
 
 // Adicionar item para lista
@@ -105,6 +118,11 @@ const updateTodo = (text) =>{
 
 }
 
+
+
+
+
+
 // Eventos
 // Evento de adicionar formulário
 todoForm.addEventListener("submit", (e)=>{
@@ -132,7 +150,7 @@ document.addEventListener("click", (e)=>{
         todoTitle = parentEl.querySelector("h3").innerText;
     }
 
-    if(targetEl.classList.contains("finished-todo")){
+    if(targetEl.classList.contains("finish-todo")){
         parentEl.classList.toggle("done");
 
     }else if(targetEl.classList.contains("remove-todo")){
@@ -142,7 +160,6 @@ document.addEventListener("click", (e)=>{
 
     }else if(targetEl.classList.contains("edit-todo")){
         toggleForms();
-
         editInput.value = todoTitle;
         oldInputValue = todoTitle;
 
@@ -160,15 +177,21 @@ cancelEditBtn.addEventListener('click', (e)=>{
 
 editForm.addEventListener('submit', (e)=>{
     e.preventDefault();
-
-
+    
     const editValue = editInput.value;
-
+    let number = localizarItem(oldInputValue);
     if(editValue){
-        updateTodo(editValue);
+        updateTodo(editValue); 
+        substituirItem(number, editValue);
 
     }
     toggleForms();
 
+});
+
+filter.addEventListener("select", (e)=>{
+    e.preventDefault();
+    // let listaOptions = document.querySelectorAll("")
+    
 
 })
