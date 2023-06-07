37b84c826e37d41146d0fcfe37027f491006b056
Leandro de Jesus <lean_silva100@hotmail.com>
2023-06-07 15:16:54 -0300

    2º commit, criado temporizador em html, estilizado e feito a funcionalidade de contagem de tempo

---
+++ README.md
@@ -0,0 +1,7 @@
+### Todo list avançado
+
+<hr>
+
+<p>Todo list avançado desenvolvido em html, css e javascript puro.
+com temporizador para marcar quantos minutos deseja estudar, além de links para linkedin e github do desenvolvedor.
+</p>
--- js/cronometro.js
+++ js/cronometro.js
@@ -1,1 +1,54 @@
 // Código javascript para criação do cronometro.
+
+// Formulário de envio do tempo.
+const formTemp = document.querySelector("#form-temporizador");
+
+// Input do temporizador
+const inputTempo = document.querySelector("#form-temp");
+
+// Função temporizador;
+const temporizador = (duracao,display)=>{
+
+    var timer = duracao, minutos, segundos;
+
+    setInterval(()=>{
+
+        minutos = parseInt(timer /60, 10);
+        segundos = parseInt(timer % 60, 10);
+
+        minutos = minutos < 10 ? "0" + minutos : minutos;
+        segundos = segundos < 10 ? "0" + segundos : segundos;
+
+        display.textContent = minutos + ":" + segundos;
+
+        if(--timer < 0 ){
+            timer = duracao;
+
+        }
+        
+    }, 1000);
+
+};
+
+
+// Capturar evento de envio de formulário.
+formTemp.addEventListener("submit", (e)=>{
+    e.preventDefault();
+
+    let tempo = 60 * parseInt(inputTempo.value); //Conversão para segundos
+    let display = document.querySelector("#tempo");
+
+    if(tempo){
+        // Enviar para temporizador.
+        temporizador(tempo, display);
+        inputTempo.value = '';
+
+    }else{
+        // não há numero na função
+        window.alert("erro ao enviar submit");
+        inputTempo.value = '';
+
+    }
+
+
+})
